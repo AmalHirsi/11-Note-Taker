28e4af94ee161d2870ae5481cb981e56e59c27ac
Amal Hirsi <ahirsi@hotmail.co.uk>
2022-09-20 22:51:42 +0100

    Final changes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const apiRoute = require("./routes/apiRoutes");
+const htmlRoute = require("./routes/htmlRoutes");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoute);
+app.use('/', htmlRoute);
+
+app.listen(PORT, () => {
+    console.log('Listening on PORT ${PORT}.');
+});
